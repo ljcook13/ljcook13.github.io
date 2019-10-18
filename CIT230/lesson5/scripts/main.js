c96e52e908a6f78a6490127de401e644e9d6863f
ljcook13 <kuh16002@byui.edu>
2019-10-18 10:39:31 -0600

    forecast days and overlay picture

--- CIT230/lesson5/scripts/main.js
+++ CIT230/lesson5/scripts/main.js
@@ -50,11 +50,19 @@ function toggleMenu() {
 }
 
 // create five-day forecast
-let day1, day2, day3, day4, day5;
+let currentDay = weekDayNumber;
 
-if (weekDayNumber < 6) {
-    day1 = daysOfWeek[weekDayNumber + 1];
-    document.getElementById("day1").innerHTML = day1;
-    day2 = daysOfWeek[weekDayNumber + 2];
-    document.getElementById("day2").innerHTML = day2;
+for (let i = 1; i < 6; i++) {
+    //add one day to current day
+    currentDay++;
+
+    // if current day is greater than 6 (Saturday), reset it to 0
+    if (currentDay > 6) {
+        currentDay = 0;
+    }
+
+    //assign value to placeholder
+    const element = document.getElementById(`day${i}`);
+
+    element.innerHTML = daysOfWeek[currentDay];
 }
