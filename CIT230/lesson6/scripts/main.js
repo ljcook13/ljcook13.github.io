033ff659f159349e5193150d69646c028792b423
ljcook13 <kuh16002@byui.edu>
2019-10-21 10:19:53 -0600

    lesson 6 additions

---
+++ CIT230/lesson6/scripts/main.js
@@ -0,0 +1,74 @@
+var currentDate = new Date();
+var currentDateString;
+
+// get day of week
+var weekDayNumber = currentDate.getDay()
+
+var daysOfWeek = [
+    'Sunday',
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Saturday'
+];
+
+var weekDay = daysOfWeek[weekDayNumber];
+currentDateString = weekDay + ', ';
+currentDateString = currentDateString + currentDate.getDate();
+
+var months = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December'
+];
+
+var monthNumber = currentDate.getMonth();
+
+var month = months[monthNumber];
+
+currentDateString += ' ' + month;
+currentDateString += ' ' + currentDate.getFullYear();
+
+document.getElementById('currentDate').innerHTML = currentDateString;
+
+//DEBUG:
+console.log(currentDateString);
+
+function toggleMenu() {
+    document.getElementsByClassName("navigation")[0].classList.toggle("responsive");
+}
+
+// create five-day forecast
+let currentDay = weekDayNumber;
+
+for (let i = 1; i < 6; i++) {
+    //add one day to current day
+    currentDay++;
+
+    // if current day is greater than 6 (Saturday), reset it to 0
+    if (currentDay > 6) {
+        currentDay = 0;
+    }
+
+    //assign value to placeholder
+    const element = document.getElementById(`day${i}`);
+
+    element.innerHTML = daysOfWeek[currentDay];
+}
+
+// show/hide pancake
+if (weekDayNumber === 5) {
+    document.getElementById('pancake').removeAttribute("class", "hidden");
+}
+
