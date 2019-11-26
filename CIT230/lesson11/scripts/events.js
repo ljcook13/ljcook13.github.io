70e94a11fcd0ba9b31a43a8250aef6f892d514f6
ljcook13 <kuh16002@byui.edu>
2019-11-25 19:06:21 -0700

    events

---
+++ CIT230/lesson11/scripts/events.js
@@ -0,0 +1,62 @@
+const eventURL = 'https://byui-cit230.github.io/weather/data/towndata.json';
+
+fetch(eventURL)
+    .then(response => {
+        response.json()
+        .then(
+            response => {
+                const eventTowns = response.towns;
+                eventTowns.forEach(
+                    (eventTown) => {
+                        if (eventTown.name.toLowerCase() === 'preston') {
+                            document.querySelector('#prestonEvents')
+                                .textContent = eventTown.events;
+                        }
+                    }
+                )
+            }
+        )
+    }
+
+)
+
+fetch(eventURL)
+    .then(response => {
+        response.json()
+        .then(
+            response => {
+                const eventTowns = response.towns;
+                eventTowns.forEach(
+                    (eventTown) => {
+                        if (eventTown.name.toLowerCase() === 'fish haven') {
+                            document.querySelector('#fishHavenEvents')
+                                .textContent = eventTown.events;
+                        }
+                    }
+                )
+            }
+        )
+    }
+
+)
+
+fetch(eventURL)
+    .then(response => {
+        response.json()
+        .then(
+            response => {
+                const eventTowns = response.towns;
+                eventTowns.forEach(
+                    (eventTown) => {
+                        if (eventTown.name.toLowerCase() === 'soda springs') {
+                            document.querySelector('#sodaSpringsEvents')
+                                .textContent = eventTown.events;
+                        }
+                    }
+                )
+            }
+        )
+    }
+
+)
+
